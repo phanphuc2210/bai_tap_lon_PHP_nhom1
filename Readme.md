ae5b46f39a7c9119c14bf8dd44119a4b8d59827a
phanphuc2210 <phucpth2001@gmail.com>
2022-10-18 19:40:40 +0700

    init

---
+++ Readme.md
@@ -0,0 +1,19 @@
+# Bài tập lớn Phần Mềm Mã Nguồn Mở PHP (Thi cuối kỳ) - Nhóm 1
+
+## Thành viên
+* Phan Trần Hữu Phúc (Phúc cuto)
+* Phạm Minh Hoàng (Hoàng cuteo)
+
+## Yêu cầu từ giảng viên
+![yeucau](https://i.imgur.com/648ra8e.jpg)
+
+## Công nghệ sử dụng
+- PHP
+- MySQL
+- HTML/CSS, Javascript
+- Bootstrap
+
+## Phân công
+1. làm gì đó
+2. Làm gì ta
+3. làm gì không biết
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php # Script 3.4 - index.php
+$page_title = 'Welcome to this Site!';
+include ('includes/header.html');
+?>
+<div class="container h-100 d-flex flex-column justify-content-center align-items-center text-white">
+    <h1>Bài tập lớn Phần Mềm Mã Nguồn Mở (Thi cuối kỳ) - Nhóm 1</h1>
+    <h3>Phan Trần Hữu Phúc</h3>
+    <h3>Phạm Minh Hoàng</h4>
+</div>
+<?php
+include ('includes/footer.html');
+?>
